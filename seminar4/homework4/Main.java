af47b91d19593882d460c32fc024463c4a4666dc
Druce <raa-07@mail.ru>
2023-02-28 21:34:44 +0300

    seminar4

---
+++ seminar4/homework4/Main.java
@@ -0,0 +1,12 @@
+package homework4;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        MainView.MainView();
+
+    }
+}
---
+++ seminar4/homework4/Priority.java
@@ -0,0 +1,7 @@
+package homework4;
+
+public enum Priority {
+    ROUTINE,
+    IMPORTANT,
+    ASAP
+}
