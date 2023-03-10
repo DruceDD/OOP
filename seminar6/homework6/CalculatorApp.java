ee1a71b4d6c0ba0d227fcf297243983a0fcc538e
Druce <raa-07@mail.ru>
2023-03-10 11:27:19 +0300

    seminar6

---
+++ seminar6/homework6/CalculatorApp.java
@@ -0,0 +1,21 @@
+package seminar6.homework6;
+
+import seminar6.homework6.gui.MainGUI;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class CalculatorApp {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        Logger logger = Logger.getLogger(CalculatorApp.class.getName());
+
+        FileHandler fh = new FileHandler("log.log");
+        logger.addHandler(fh);
+
+        SimpleFormatter sFormat = new SimpleFormatter();
+        fh.setFormatter(sFormat);
+
+        MainGUI.MainGUI(logger);
+    }
+}
